2a97d3b914265c6e04278fd56f03fc641c84de34
mgutierrez93 <mbg993@gmail.com>
2016-10-18 23:41:00 -0500

    Create 2d.rb

---
+++ 2d.rb
@@ -0,0 +1,27 @@
+require "./methods.rb"
+
+counter = 0.0
+
+File.open('http_access_log', 'r') do |f|
+
+	f.each do |line|
+
+		if line.ruby_safeguard
+
+			next
+
+		end
+
+		if	time = line[/ 3\d\d/]
+
+			counter += 1
+
+		end
+
+	end
+
+end
+
+precent = (counter / 726739) * 100
+
+puts "The percentage of requests that were redirected eslewhere is #{precent.round(2)}%"
